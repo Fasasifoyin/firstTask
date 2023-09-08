9b574926df212b3d16d5685698df64aa087227df
foyinjohn <fasasifoyin@gmail.com>
2023-09-07 19:00:46 -1000

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api", (req, res) => {
+  const { slack_name, track } = req.query;
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const date = new Date();
+  const day = date.getDay();
+  const utc_time = date.toISOString();
+
+  const result = {
+    slack_name: slack_name,
+    current_day: dayNames[day],
+    utc_time,
+    track: track,
+    status_code: 200,
+  };
+
+  res.status(200).json(result);
+});
+
+app.listen(8080, () => console.log("Running..."));
